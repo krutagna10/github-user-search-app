985706d240caa3891ace1e2aa034966f5eebebf9
Krutagna Patel <kpatel.kp7016@gmail.com>
2023-09-20 01:04:21 +0530

    added some updates

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,9 @@
-import iconSun from "../../assets/icon-sun.svg";
 import "./Header.css";
 
 function Header() {
   return (
-    <header className="header flex justify-between">
-      <h1 className="fs-400">devfinder</h1>
-      <button className="header__btn" aria-label="Toggle Theme">
-        <img src={iconSun} alt="" />
-      </button>
+    <header className="header">
+      <h1 className="fs-400 text-center">devfinder</h1>
     </header>
   );
 }
